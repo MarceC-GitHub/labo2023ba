58e303ef557d4bb7f13ddc8dd8b600f72db687bc
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-06 08:55:52 -0300

    Cambios

--- src/lightgbm/424_lightgbm_final_V1.r
+++ src/lightgbm/424_lightgbm_final_V1.r
@@ -20,14 +20,14 @@ PARAM$input$dataset <- "./datasets/dataset_pequeno.csv"
 PARAM$input$training <- c(202107) # meses donde se entrena el modelo
 PARAM$input$future <- c(202109) # meses donde se aplica el modelo
 
-PARAM$finalmodel$semilla <- 123457      
+PARAM$finalmodel$semilla <- 102191
 
 PARAM$finalmodel$num_iterations <- 559
 PARAM$finalmodel$learning_rate <- 0.0100746999
 PARAM$finalmodel$feature_fraction <- 0.5144127527
 PARAM$finalmodel$min_data_in_leaf <- 505
 PARAM$finalmodel$num_leaves <- 44
-#### Con parámetros dados por el  profesor
+
 
 PARAM$finalmodel$max_bin <- 31
 
