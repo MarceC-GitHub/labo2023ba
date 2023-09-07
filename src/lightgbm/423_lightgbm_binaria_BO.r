46175be69020ac8c16fae9c8e35ba182d95b33ec
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-07 15:25:27 -0300

    Prepare V0

--- src/lightgbm/423_lightgbm_binaria_BO.r
+++ src/lightgbm/423_lightgbm_binaria_BO.r
@@ -1,6 +1,6 @@
 # Este script esta pensado para correr en Google Cloud
 #   8 vCPU
-#  16 GB memoria RAM
+ 16 GB memoria RAM
 
 # se entrena con clase_binaria2  POS =  { BAJA+1, BAJA+2 }
 # Optimizacion Bayesiana de hiperparametros de  lightgbm,
--- src/workflow-inicial/661_ZZ_final_V0.r
+++ src/workflow-inicial/661_ZZ_final_V0.r
@@ -43,7 +43,7 @@ PARAM$exp_input <- "HT6510"
 PARAM$modelos_rank <- c(1)
 
 # reemplazar por las propias semillas
-PARAM$semillas <- c(102191, 200177, 410551, 552581, 892237)
+PARAM$semillas <- c(123457, 150523, 370003, 737797, 910003)
 
 PARAM$kaggle$envios_desde <- 9500L
 PARAM$kaggle$envios_hasta <- 11500L
