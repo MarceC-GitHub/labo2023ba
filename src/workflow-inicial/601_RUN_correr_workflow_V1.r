de6676ecba403cb7c9577c848d6a659b4769141e
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-11 18:34:32 -0300

    Update 601_RUN_correr_workflow_V1.r

--- src/workflow-inicial/601_RUN_correr_workflow_V1.r
+++ src/workflow-inicial/601_RUN_correr_workflow_V1.r
@@ -18,3 +18,7 @@ source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_V1.r")
 # ultimos pasos, muy lentos
 source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_V1.r")
 source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_V1.r")
+
+# Cambio vs. 6X0
+# varia de 0.0 a 2.0, si es 0.0 NO se activan
+PARAM$CanaritosAsesinos$ratio <- 1.0
