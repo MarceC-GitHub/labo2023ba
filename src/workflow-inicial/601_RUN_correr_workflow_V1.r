99d8d3f256d5d8811592bc5b25c2df64f59e9c4b
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-12 17:17:51 -0300

    Update 601_RUN_correr_workflow_V1.r

--- src/workflow-inicial/601_RUN_correr_workflow_V1.r
+++ src/workflow-inicial/601_RUN_correr_workflow_V1.r
@@ -18,3 +18,9 @@ source("~/labo2023ba/src/workflow-inicial/z641_TS_training_strategy_V1.r")
 # ultimos pasos, muy lentos
 source("~/labo2023ba/src/workflow-inicial/z651_HT_lightgbm_V1.r")
 source("~/labo2023ba/src/workflow-inicial/z661_ZZ_final_V1.r")
+
+### Cambios a V0
+# varia de 0.0 a 2.0, si es 0.0 NO se activan
+PARAM$CanaritosAsesinos$ratio <- 1.0
+#  "ninguno", "rank_simple", "rank_cero_fijo", "deflacion", "estandarizar"
+PARAM$metodo <- "rank_cero_fijo"
