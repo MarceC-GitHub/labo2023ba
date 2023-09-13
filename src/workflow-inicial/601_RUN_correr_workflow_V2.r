5cb0ff806b150620a75c8dff0b455467039ed54d
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-13 16:19:12 -0300

    Update 601_RUN_correr_workflow_V2.r

--- src/workflow-inicial/601_RUN_correr_workflow_V2.r
+++ src/workflow-inicial/601_RUN_correr_workflow_V2.r
@@ -11,7 +11,7 @@ options(error = function() {
 
 # primeros pasos, relativamente rapidos
 source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_V2.r")
-source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_V2".r")
+source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_V2.r")
 source("~/labo2023ba/src/workflow-inicial/631_FE_historia_V2.r")
 source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy.r")
 
