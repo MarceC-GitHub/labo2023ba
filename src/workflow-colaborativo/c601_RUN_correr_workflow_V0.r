ebd537e3613b44983c7ab15eba44249544d67a5a
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-14 22:08:15 -0300

    Name Change

---
+++ src/workflow-colaborativo/c601_RUN_correr_workflow_V0.r
@@ -0,0 +1,20 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/labo2023ba/src/workflow-inicial/c611_CA_reparar_dataset_0.r")
+source("~/labo2023ba/src/workflow-inicial/c621_DR_corregir_drifting_V0.r")
+source("~/labo2023ba/src/workflow-inicial/c631_FE_historia_V0.r")
+source("~/labo2023ba/src/workflow-inicial/c641_TS_training_strategy_V0.r")
+
+# ultimos pasos, muy lentos
+source("~/labo2023ba/src/workflow-inicial/c651_HT_lightgbm_V0.r")
+source("~/labo2023ba/src/workflow-inicial/c661_ZZ_final_V0.r")
