0be1551b410e1fd77ac3c6b465f6e00c1326e70f
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-15 16:03:17 -0300

    Update c601_RUN_correr_workflow_V1.r

--- src/workflow-colaborativo/c601_RUN_correr_workflow_V1.r
+++ src/workflow-colaborativo/c601_RUN_correr_workflow_V1.r
@@ -18,3 +18,5 @@ source("~/labo2023ba/src/workflow-colaborativo/c641_TS_training_strategy_V1.r")
 # ultimos pasos, muy lentos
 source("~/labo2023ba/src/workflow-colaborativo/c651_HT_lightgbm_V1.r")
 source("~/labo2023ba/src/workflow-colaborativo/c661_ZZ_final_V1.r")
+
+# Cambie future to 202107
