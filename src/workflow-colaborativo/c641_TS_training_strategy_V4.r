1996a300148a23aa525a80d8d6fb6a3491715d9a
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-23 15:26:40 -0300

    Update c641_TS_training_strategy_V4.r

--- src/workflow-colaborativo/c641_TS_training_strategy_V4.r
+++ src/workflow-colaborativo/c641_TS_training_strategy_V4.r
@@ -33,8 +33,8 @@ PARAM$train$training <- c(
   201910, 201909, 201908, 201907, 201906, 201905
 )
 
-PARAM$train$validation <- c(202105)
-PARAM$train$testing <- c(202108, 202107,202106)
+PARAM$train$validation <- c(202104)
+PARAM$train$testing <- c(202107, 202106, 202104)
 
 # Atencion  0.1  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
