01e41a641558defcd1f917152c1a527d6b76bbb8
MarceC-GitHub <60663305+MarceC-GitHub@users.noreply.github.com>
2023-09-25 18:19:29 -0300

    Update

--- src/workflow-colaborativo/new/661_ZZ_final_v3.r
+++ src/workflow-colaborativo/new/661_ZZ_final_v3.r
@@ -36,7 +36,7 @@ require("lightgbm")
 # Parametros del script
 PARAM <- list()
 PARAM$experimento <- "zZZ6613"
-PARAM$exp_input <- "<HT6513"
+PARAM$exp_input <- "zHT6513"
 
 # Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades
 # Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
